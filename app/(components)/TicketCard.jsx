0788ef762cb1823be84d980dbc51ef5059d83736
Marcus Ngugi <marcusngugi@gmail.com>
2024-07-26 13:55:41 +0300

    "Updated TicketCard component layout and added styles to globals.css"

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -17,8 +17,16 @@ const TicketCard = () => {
       <p className="whitespace-pre-wrap">
         Here is the ticket description. Please do this ticket!
       </p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">01/07/24 10:44AM</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
