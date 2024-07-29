5d819ff385ef5e51a7dfb9ecd37e5dde916ab235
Marcus Ngugi <marcusngugi@gmail.com>
2024-07-29 23:03:10 +0300

    Refactor ticketPage component to fetch ticket data by ID and update form for editing existing tickets

--- app/ticketPage/[id]/page.jsx
+++ app/ticketPage/[id]/page.jsx
@@ -1,6 +1,26 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
-const ticketPage = () => {
+const getTicketById = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to get Ticket.");
+  }
+
+  return res.json();
+};
+
+const ticketPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+    console.log(updateTicketData);
+  }
+
   return <TicketForm />;
 };
 
